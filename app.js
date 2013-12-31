82f25f9ec09cdf8f4b156baa91fbdf60b2d2dc80
wshert1688 <wsh1688@163.com>
2013-12-31 14:41:46 +0800

    ws

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get('/', routes.index);
 app.get('/prt', routes.programRT);
 app.get('/mvcrt', routes.mvcRT);
 app.get('/cd', routes.chartData);
+app.get('/md', routes.mainData);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -25,18 +25,50 @@ exports.index = function (req, res) {
 };
 
 exports.programRT = function (req, res) {
-    client.zrevrangebyscore(['programRT', 99999, 0, 'LIMIT', 0, 99999],
+    res.render('programRT');
+};
+exports.mainData = function (req, res) {
+    var k = req.query.k;
+    var s = req.query.s;
+    client.zrevrangebyscore([k, 99999, 0, 'LIMIT', 0, 99999],
         function (err, data) {
             async.map(data, function (t, cb) {
-                client.hgetall('programRT' + t, function (err, d) {
+                client.hgetall(k + t, function (err, d) {
+
+                    d["key"] = t;
                     cb(err, d);
                 });
             }, function (err, results) {
-                res.render('programRT', {  d: data, l: results });
+                if (s == "max") {
+                    results = comb.array.sort(results, function (a, b) {
+                        return a.max - b.max;
+                    })
+                }
+                if(s == "min"){
+                    results = comb.array.sort(results, function (a, b) {
+                        return a.min - b.min;
+                    })
+                }
+                if(s == "call"){
+                    results = comb.array.sort(results, function (a, b) {
+                        return a.call - b.call;
+                    })
+                }
+                if(s == "key"){
+                    results = comb.array.sort(results, function (a, b) {
+                        return a.key.length - b.key.length;
+                    })
+                }
+                if(s == "avg"){
+                    results = comb.array.sort(results, function (a, b) {
+                        return a.avg - b.avg;
+                    })
+                }
+                res.send(200, {  l: results.reverse() });
             });
         }
     )
-};
+}
 exports.chartData = function (req, res) {
     var keys = req.query.keys.split(",");
     var sql = "select `avg` from perf.perf where `key`=? order by `date` desc limit 90;";
@@ -52,7 +84,7 @@ exports.chartData = function (req, res) {
             for (var o in results[r].d) {
                 arr.push(results[r].d[o].avg);
             }
-            h[results[r].k.replace(" ","")] = arr.reverse();
+            h[results[r].k.replace(" ", "")] = arr.reverse();
 
         }
         res.send(200, { l: h });
